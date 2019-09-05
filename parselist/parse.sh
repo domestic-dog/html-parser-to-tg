70b4c78d15a941e12777e7dd55722fa645ecc2c0
Dmitry <dmitrypodsytnik@gmail.com>
2019-09-05 09:54:42 +0300

    add a pup package + remove lists

--- parselist/parse.sh
+++ parselist/parse.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+rm -f termlist
 curl -s -d  '{login:"d.podsytnik", password:"81dc9bdb52d04dc20036dbd8313ed055"}' -c cookie  -H "Content-Type:application/json" -X POST  https://v2.vendingloto.ru/api/login/index
-curl -s -b cookie -o list1.html  https://v2.vendingloto.ru/terminal/terminallist
-curl -s -b cookie -o list2.html  https://v2.vendingloto.ru/terminal/terminallist?pagenumber=1
-curl -s -b cookie -o list3.html  https://v2.vendingloto.ru/terminal/terminallist?pagenumber=2
-curl -s -b cookie -o list4.html  https://v2.vendingloto.ru/terminal/terminallist?pagenumber=3
+curl -s -b cookie -o file#1  https://v2.vendingloto.ru/terminal/terminallist?pagenumber=[0-4];echo ''
+cat file* >> termlist;rm file*
