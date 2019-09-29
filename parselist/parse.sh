99ed9b17975e61f76352e6589176f17254174a83
Dmitry <dmitrypodsytnik@gmail.com>
2019-09-29 22:18:18 +0300

    GotIt

--- parselist/parse.sh
+++ parselist/parse.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 rm  termlist
 curl -s -d  '{login:"d.podsytnik", password:"81dc9bdb52d04dc20036dbd8313ed055"}' -c cookie  -H "Content-Type:application/json" -X POST  https://v2.vendingloto.ru/api/login/index
 curl -s -b cookie -o file#1  https://v2.vendingloto.ru/terminal/terminallist?pagenumber=[0-6];echo ''
 cat file* |pup 'td'| sed '/td/d' | sed '/btn/d' | sed '/fa fa-info-circle/d'| sed '/fa fa-pencil/d'| sed '/d>/d'| sed '/a>/d'| sed '/i>/d'|  sed '/span/d' >> termlist;rm file*
-cat termlist
+#cat termlist
--- valitade.py
+++ valitade.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
-import psycopg2
+import subprocess
+import csv
 
-conn = psycopg2.connect(dbname='listdb', user='worker')
-cursor = conn.cursor()
+data = {}
 
-
-
-
-
-
-
-
-#def update_teminaltb(term_name,agent,adress,uptime,money,status):
-#    sql = """ UPDATE terminaltb SET id = %s, SET term_name = %s,SET agent = %s, SET adress = %s,SET uptime = %s, SET status = %s WHERE id = %s """
-#    cursor.execute(sql, (result[id],result[term_name],result[agent],result[adress],result[uptime],result[money],result[status])
+with open('temp.csv','r') as fin:
+    reader=csv.reader(fin, skipinitialspace=True, quotechar="'") 
+    for row in reader:
+        data[row[0]] =row[1]
+print(data)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+subprocess.call("./parselist/parse.sh")
+
+#_________get cvs
+
+
+#openfile
 file_to_open = "/home/worker/vendingautm/parselist/termlist"
 f = open(file_to_open)
-class Terminal:
-  def __init__(self, id,term_name,agent,adress,uptime,money,status):
-    self.id = id
-    self.term_name = term_name
-    self.agent = agent
-    self.adress = adress
-    self.uptime = uptime
-    self.money = money
-    self.status = status
 
-
-i=1
 id = 0
 term_name = 1
 agent = 2
@@ -38,26 +40,27 @@ adress = 3
 uptime = 4
 money = 5
 status = 6
-file = open('temp.txt', 'w')
+newdata = {}
+file = open('temp.csv', 'w')
 result =f.read().splitlines()
-count = len(result)
-print(count)
-for i in  result(count) : 
-    print(result[id] + result[status])
-    #str = Terminal(result[id],result[term_name],result[agent],result[adress],result[uptime],result[money],result[status])
-    #querty = """ INSERT INTO test2 (id, term_name, agent, adress, uptime, money, status) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
-    #datainsert = [result[id],result[term_name],result[agent],result[adress],result[uptime],result[money],result[status],result[id]]
-    #sql = """ UPDATE test2 SET 'id' = %s, SET 'term_name' = %s, SET 'agent' = %s, SET 'adress',SET 'uptime'= %s, SET 'money' =%s, SET 'status' = %s WHERE 'id' = %s """
-    #cursor.execute(sql, (result[id],result[term_name],result[agent],result[adress],result[uptime],result[money],result[status],result[id]))
-    #cursor.execute(sql, datainsert)
-    #conn.commit()
-    id+=7
-    status+= 7
-    term_name+=7
-    agent+=7
-    uptime+=7
-    money+=7
-    adress+=7
-    i=+1
-    #file.write(str.id+ str.term_name+ str.agent + str.status + '\n')
-    #print(str.__dict__)
+while  result :
+    try:
+        #print(result[id] + result[status])
+        file.write(result[id] +',' + result[status].lstrip()  + '\n' )
+        newdata[result[id]] =result[status].lstrip() 
+        id+=7
+        status+= 7
+        term_name+=7
+        agent+=7
+        uptime+=7
+        money+=7
+        adress+=7
+        # file.write(result[id] +',' + result[status].lstrip()  + '\n' )
+    except IndexError:
+        break;
+
+
+#End of get infoList
+
+
+
